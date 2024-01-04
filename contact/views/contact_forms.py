f7f99021ffb98aefbcddc444815234b8225ed3f7
CR7sewey <miguelmotacarvalho@gmail.com>
2024-01-04 16:51:57 +0000

    request.method and request.POST to check when forms is posted

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -7,9 +7,16 @@ from django.http import Http404, HttpRequest, HttpResponse
 
 def create(request):
 
-    # context = {"page_obj": page_obj, "site_title": 'Contacts - '}
-    return render(
-        request,
-        'contact/create.html',
-        {}
-    )
+    if request.method == 'POST':  # pq pode ser get a vir de la tmb!
+        # From form in create.html
+        first_name = request.POST.get('first_name').strip()
+        last_name = request.POST.get('first_name', '').strip()
+
+        # context = {"page_obj": page_obj, "site_title": 'Contacts - '}
+        return render(
+            request,
+            'contact/create.html',
+            {}
+        )
+    else:  # GET!
+        ...
