4b9f6599fd9485c7ff254e5edf79b7ad0cb480cd
CR7sewey <miguelmotacarvalho@gmail.com>
2024-01-04 15:21:31 +0000

    New organization pattern on urls

--- contact/urls.py
+++ contact/urls.py
@@ -5,7 +5,13 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
-    path('<int:contact_id>/', views.contact, name='contact'),
-    path('search/', views.search, name='search'),
     path('', views.index, name='index'),
+    path('search/', views.search, name='search'),
+
+    # contact (CRUD)
+    path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
+    # path('contact//create/', views.contact, name='contact'),  # no id yet
+    # path('contact/<int:contact_id>/update/', views.contact, name='contact'),
+    # path('contact/<int:contact_id>/delete/',
+    #  views.contact, name='contact'),  # dangerous!
 ]
