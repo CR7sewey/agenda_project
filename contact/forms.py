bebdc7c066833fe15434aaf7f81e6f81bbb1fafd
CR7sewey <miguelmotacarvalho@gmail.com>
2024-01-05 15:27:14 +0000

    Fields validation with clean, clean_field and ValidationError

--- contact/forms.py
+++ contact/forms.py
@@ -41,15 +41,35 @@ class ContactForm(forms.ModelForm):
         #           'placeholder': 'Write here'}  # renderiza classe no html!
         # )}
 
-    def clean(self):  # sobrescrever metodo
-        # cleaned_data = self.cleaned_data  # data before added to database
+    def clean(self):  # sobrescrever metodo, used to validate fields!
+        cleaned_data = self.cleaned_data  # data before added to database
 
-        self.add_error(
-            'first_name',  # or first_name for ex - atrelar a um campo
-            ValidationError('Error 1:', code='invalid')
-        )
+        # self.add_error(
+        #   'first_name',  # or first_name for ex - atrelar a um campo
+        #   ValidationError('Error 1:', code='invalid')
+        # )
 
-        self.add_error(
-            'first_name',  # or first_name for ex - atrelar a um campo
-            ValidationError('Error 2:', code='invalid')
-        )
+        # self.add_error(
+        #    'first_name',  # or first_name for ex - atrelar a um campo
+        #    ValidationError('Error 2:', code='invalid')
+        # )
+
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+        if first_name == last_name:
+            self.add_error('first_name',
+                           ValidationError('First name must be different from Last name', code='invalid'))
+            self.add_error('last_name',
+                           ValidationError('First name must be different from Last name', code='invalid'))
+
+        return super().clean()
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+        if first_name == 'ABC':
+            self.add_error(  # or raise (o problema do raise é que dps nao deixa ver os restantes erros que possam haver!!)
+                'first_name',  # or first_name for ex - atrelar a um campo
+                ValidationError('Add error', code='invalid')
+            )
+
+        return first_name
