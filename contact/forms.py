6bf9eeb3095499beca43d83d5f607f8b1675c455
CR7sewey <miguelmotacarvalho@gmail.com>
2024-01-05 15:14:10 +0000

    Widgets in form

--- contact/forms.py
+++ contact/forms.py
@@ -4,11 +4,42 @@ from . import models
 
 
 class ContactForm(forms.ModelForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'class-a class-b',
+                                      'placeholder': 'Write here'}  # renderiza classe no html!
+                               ),
+        help_text='Helpful text for user',
+        label='First Name'
+    )
+
+    # criar campo aqui sem ser no models!
+    # qualquer = forms.CharField(
+    #    widget=forms.TextInput(attrs={'class': 'class-a class-b',
+    #                                 'placeholder': 'Write here'}  # renderiza classe no html!
+    #                          ),
+    #   help_text='Helpful text for user',
+    # )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # self.fields['first_name'].widget.attrs.update({
+        #    'placeholder': 'Write here'
+        # })  # atualizar o wideget que ja estava no campo
+
     class Meta:
         model = models.Contact
         fields = ('first_name', 'last_name',
                   'phone',)  # , 'email', 'description',
         # 'show', 'picture', 'category', 'owner',)
+
+        # https://docs.djangoproject.com/en/5.0/ref/forms/widgets/
+        # campo a ser renderizado! ex forms.PasswordInput()
+        # criar novo widget!
+        # widgets = {'first_name': forms.TextInput(
+        #    attrs={'class': 'class-a class-b',
+        #           'placeholder': 'Write here'}  # renderiza classe no html!
+        # )}
 
     def clean(self):  # sobrescrever metodo
         # cleaned_data = self.cleaned_data  # data before added to database
